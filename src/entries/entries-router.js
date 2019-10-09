b29a55557989c5151997b2d8f6f6b27e4e127d2e
Caroline Marsden <car.marsden@gmail.com>
2019-10-09 12:32:10 -0400

    Add initial parties post endpoint

--- src/entries/entries-router.js
+++ src/entries/entries-router.js
@@ -20,4 +20,51 @@ entriesRouter
     })
 ;
 
+entriesRouter
+    .route('/parties')
+    .post(jsonParser, (req, res, next) => {
+        const body = req.body;
+        body.user_id = Number(req.query.user_id);
+
+        // validate requirements: check for required values
+        const required = ['party_name', 'reputation'];
+        for (const requirement of required) {
+            if (!body[requirement]) {
+                logger.error(`Missing '${requirement}' in party post request`);
+                return res.status(400).json({
+                    error: `Missing '${requirement}' in request body`
+                })
+            }
+        }
+
+        // validate reputation: must be an integer from -20 to +20
+        if (typeof(body.reputation) !== 'number') {
+            body.reputation = Number(body.reputation);
+
+            if (Number.isNaN(body.reputation) || !Number.isInteger(body.reputation)) {
+                logger.error(`Reputation is not an integer in party post request`);
+                return res.status(400).json({
+                    error: `Reputation must be an integer`
+                })
+            }
+
+            if (body.reputation < -20 || body.reputation > 20) {
+                logger.error(`Reputation must be from -20 to +20`);
+                return res.status(400).json({
+                    error: `Reputation must be between -20 and +20`
+                })
+            }
+        }
+
+        // insert party
+        const party = EntriesService.serializeParty(body);
+        EntriesService.insertParty(req.app.get('db'), party)
+            .then(result => {
+                res.status(201).json(EntriesService.serializeParty(result))
+            })
+            .catch(next)
+        ;
+    })
+;
+
 module.exports = entriesRouter;
--- src/entries/entries-service.js
+++ src/entries/entries-service.js
@@ -16,6 +16,60 @@ const EntriesService = {
             .orderBy('char.xp', 'asc')
         ;
     },
+
+    insertParty(db, newParty) {
+        return db.insert(newParty)
+            .into('gloomhelper_parties')
+            .returning('*')
+            .then(([party]) => party)
+        ;
+    },
+
+    insertChar(db, newChar) {
+        return db.insert(newChar)
+            .into('gloomhelper_chars')
+            .returning('*')
+            .then(([char]) => char)
+        ;
+    },
+
+    serializeParty(party) {
+        return {
+            id: party.id,
+            user_id: party.user_id,
+            party_name: xss(party.party_name),
+            location: xss(party.location),
+            reputation: party.reputation,
+            party_notes: xss(party.party_notes),
+            achievements: xss(party.achievements),
+            date_modified: party.date_modified
+        }
+    },
+
+    serializeChar(char) {
+        return {
+            id: char.id,
+            user_id: char.user_id,
+            character_name: xss(char.character_name),
+            character_class: char.character_class,
+            xp: char.xp,
+            gold_notes: xss(char.gold_notes),
+            items_notes: xss(char.items_notes),
+            character_notes: xss(char.character_notes),
+            goals_1: char.goals_1,
+            goals_2: char.goals_2,
+            goals_3: char.goals_3,
+            goals_4: char.goals_4,
+            goals_5: char.goals_5,
+            goals_6: char.goals_6,
+            perks: xss(char.perks),
+            date_modified: char.date_modified
+        }
+    },
+
+    serializeCharArray(arr) {
+        return arr.map(this.serializeChar)
+    },
 }
 
 module.exports = EntriesService;
