1b875d71701a2076cfec4bdf97877a1e26b45eac
Caroline Marsden <car.marsden@gmail.com>
2019-10-15 12:00:17 -0400

    Update seed HTML to \n breaks

--- seeds/seed.gloomhelper_tables.sql
+++ seeds/seed.gloomhelper_tables.sql
@@ -14,12 +14,12 @@ VALUES
 ;
 
 INSERT INTO gloomhelper_parties (user_id, party_name, location, reputation, party_notes, achievements)
-    VALUES (1, 'Gloomhaven Gangsters', 'Gloomhaven', 4, 'last we checked, we were rising up against Jeksarah--pretty sure she''s evil??', '<ul><li>First Steps</li><li>Second Steps</li></ul>');
+    VALUES (1, 'Gloomhaven Gangsters', 'Gloomhaven', 4, 'last we checked, we were rising up against Jeksarah--pretty sure she''s evil??', 'First Steps\nSecond Steps');
 
 INSERT INTO gloomhelper_chars (user_id, character_name, character_class, xp, gold_notes, items_notes, character_notes, goals_1, goals_2, goals_3, perks)
 VALUES 
-    (1, 'Sal', 'brute', 32, '13 (saving up for chainmail armor)', '<ul><li>Eagle Eye Goggles</li><li>Boots of Striding</li></ul>', 'x2 blessings next scenario', 3, 1, 0, '000010000000000'),
-    (1, 'Transcendy MacMillion', 'spellweaver', 48, '46', '<ul><li>Minor Manna Potion</li></ul>', 'Elite kills = 4', 2, 0, 0, '000001000000010')
+    (1, 'Sal', 'brute', 32, '13 (saving up for chainmail armor)', 'Eagle Eye Goggles\nBoots of Striding', 'x2 blessings next scenario', 3, 1, 0, '000010000000000'),
+    (1, 'Transcendy MacMillion', 'spellweaver', 48, '46', 'Minor Manna Potion', 'Elite kills = 4', 2, 0, 0, '000001000000010')
 ;
 
 COMMIT;
