729e40a40fc20d3890c1eab06487e1dad907807f
Caroline Marsden <car.marsden@gmail.com>
2019-10-16 11:25:03 -0400

    Configure CORS origin

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
 const app = express();
 const entriesRouter = require('./entries/entries-router');
 const usersRouter = require('./users/users-router');
@@ -16,7 +16,9 @@ const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
 app.use(morgan(morganOption));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}));
 
 
 // BASIC ENDPOINT & ROUTING
