53ae380badfe19d130c5a2d23511df6915868fb8
Caroline Marsden <car.marsden@gmail.com>
2019-10-25 13:09:40 -0400

    Add tests for entries: post char happy path

--- test/entries-endpoints.spec.js
+++ test/entries-endpoints.spec.js
@@ -1,9 +1,8 @@
 const knex = require('knex');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
-const EntriesService = require('../src/entries/entries-service');
 
-describe('Entries endpoints', function() {
+describe.only('Entries endpoints', function() {
     // ESTABLISH TEST ENVIRONMENT
     let db;
     const testUsers = helpers.makeUsersArray();
@@ -60,7 +59,7 @@ describe('Entries endpoints', function() {
     })
 
     // TEST CASES: /api/entries/parties
-    describe.only('POST /api/entries/parties', () => {
+    describe('POST /api/entries/parties', () => {
         context('Happy path', () => {
             it(`responds with 201 and serialized party`, () => {
                 return supertest(app)
@@ -99,5 +98,59 @@ describe('Entries endpoints', function() {
         })
     })
 
-
+    // TEST CASES: /api/entries/characters
+    describe('POST /api/entries/characters', () => {
+        context('Happy path', () => {
+            it(`responds with 201 and serialized character`, () => {
+                return supertest(app)
+                    .post('/api/entries/characters')
+                    .set('Authorization', `Bearer ${helpers.makeAuthToken(testUser)}`)
+                    .send(testChar)
+                    .expect(201)
+                    .expect(res => {
+                        expect(res.body).to.have.property('id')
+                        expect(res.body.user_id).to.eql(testUser.expected_id)
+                        expect(res.body.character_name).to.eql(testChar.character_name)
+                        expect(res.body.character_class).to.eql(testChar.character_class)
+                        expect(res.body.xp).to.eql(testChar.xp)
+                        expect(res.body.gold_notes).to.eql(testChar.gold_notes)
+                        expect(res.body.items_notes).to.eql(testChar.items_notes)
+                        expect(res.body.character_notes).to.eql(testChar.character_notes)
+                        expect(res.body.goals_1).to.eql(testChar.goals_1)
+                        expect(res.body.goals_2).to.eql(testChar.goals_2)
+                        expect(res.body.goals_3).to.eql(testChar.goals_3)
+                        expect(res.body.goals_4).to.eql(testChar.goals_4)
+                        expect(res.body.goals_5).to.eql(testChar.goals_5)
+                        expect(res.body.goals_6).to.eql(testChar.goals_6)
+                        expect(res.body.perks).to.eql(testChar.perks)
+                        expect(res.body.date_modified).to.eql(testChar.date_modified.toISOString())
+                    })
+                    .expect(res =>
+                        db
+                            .from('gloomhelper_chars')
+                            .select('*')
+                            .where({ id: res.body.id })
+                            .first()
+                            .then(row => {
+                                expect(row.user_id).to.eql(testUser.expected_id)
+                                expect(row.character_name).to.eql(testChar.character_name)
+                                expect(row.character_class).to.eql(testChar.character_class)
+                                expect(row.xp).to.eql(testChar.xp)
+                                expect(row.gold_notes).to.eql(testChar.gold_notes)
+                                expect(row.items_notes).to.eql(testChar.items_notes)
+                                expect(row.character_notes).to.eql(testChar.character_notes)
+                                expect(row.goals_1).to.eql(testChar.goals_1)
+                                expect(row.goals_2).to.eql(testChar.goals_2)
+                                expect(row.goals_3).to.eql(testChar.goals_3)
+                                expect(row.goals_4).to.eql(testChar.goals_4)
+                                expect(row.goals_5).to.eql(testChar.goals_5)
+                                expect(row.goals_6).to.eql(testChar.goals_6)
+                                expect(row.perks).to.eql(testChar.perks)
+                                expect(row.date_modified).to.eql(testChar.date_modified)
+                            })
+                    )
+                ;
+            })
+        })
+    })
 })
